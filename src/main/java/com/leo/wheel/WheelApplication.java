8288325d76e73130a4e952c70bdbe21557ec9f74
zhaoleo <zhaoleoboy@163.com>
2019-01-22 17:56:56 +0800

    init project

---
+++ src/main/java/com/leo/wheel/WheelApplication.java
@@ -0,0 +1,17 @@
+package com.leo.wheel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class WheelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelApplication.class, args);
+	}
+	
+
+}
+
