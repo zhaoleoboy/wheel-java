b9ef34f6b4ee3332e6ae2602c7948eb91239a7f4
XIAO1059 <zhaoleoboy@163.com>
2019-03-04 22:10:24 +0800

    added jedis

---
+++ src/test/java/com/leo/wheel/db/RedisTest.java
@@ -0,0 +1,39 @@
+package com.leo.wheel.db;
+
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * redis测试类，参考：https://www.cnblogs.com/byxxw/p/5822589.html
+ * 
+ * @author leo
+ *
+ */
+public class RedisTest {
+
+	private Jedis getJedis() {
+		Jedis jedis = new Jedis("47.244.165.23", 6379);
+		return jedis;
+	}
+
+	@Test
+	public void test_string_set() {
+		Jedis jedis = getJedis();
+		jedis.set("age", "1000");
+		System.out.println(jedis.get("age"));
+	}
+
+	@Test
+	public void test_strign_setex() {
+		Jedis j = getJedis();
+		j.setex("customer", 2, "张铭");
+		System.out.println("customer:" + j.get("customer"));
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("customer sleep 3 second =" + j.get("customer"));
+	}
+}
