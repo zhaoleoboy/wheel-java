b8b221b343298e8ce0a41ca994bbfed0ea09cb29
zhaoleo <zhaoleoboy@163.com>
2019-03-05 12:32:59 +0800

    add mvn skip test

---
+++ quick-start.bat
@@ -0,0 +1,21 @@
+﻿@echo off
+echo [Pre-Requirement] Makesure install JDK 8.0+ and set the JAVA_HOME.
+echo [Pre-Requirement] Makesure install Maven 3.0.3+ and set the PATH.
+
+set MVN=mvn
+set MAVEN_OPTS=%MAVEN_OPTS% -XX:MaxPermSize=128M
+
+echo [Step 1] Install all springside modules to local maven repository.
+call %MVN% clean install
+if errorlevel 1 goto error
+
+echo [Step 2] run wheel project in dev mode.
+echo 不执行测试用例，也不编译测试用例类。
+mvn spring-boot:run -Dspring.profiles.active=prod
+if errorlevel 1 goto error
+
+goto end
+:error
+echo Error Happen!!
+:end
+pause
--- src/main/java/com/leo/wheel/Main.java
+++ src/main/java/com/leo/wheel/Main.java
@@ -1,9 +1,11 @@
 package com.leo.wheel;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan()
 public class Main {
 	
 	public static void main(String[] args) {
